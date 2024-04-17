06ce9c8ec47ac9cb97000c4263ff7c551a0ffa12
Selisine <Aidenpike113@gmail.com>
2024-04-17 19:54:30 +0000

    Add suggestion comment

--- src/include/Alex.hpp
+++ src/include/Alex.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 namespace final {
+  //Unless you wanna make multiple variations of blackjack, I would just recommend a couple functions that allows the player to play blackjack, rather than a whole class. --Aiden P
   class black{
   public:
 
