68cf0b231bb94c59cf0931d1bbe2642dc839a8a4
aidenpike <aidenpike113@gmail.com>
2024-04-24 19:27:00 +0000

    Define roulette(), fix generate_random_number()

--- src/Games.cpp
+++ src/Games.cpp
@@ -3,11 +3,20 @@
 namespace final {
     class Games {
         int generate_random_number(int minimum, int maximum){
-            srand(int(time(NULL)));
-
             int random_number = rand() % (maximum - minimum + 1) + minimum;
 
             return random_number;
+        }
+
+        void roulette(){
+            srand(int(time(NULL)));
+            std::string icons[8] = {"Cherry", "Banana", "7", "Kings", "Bar", "Dollar Sign", "Money Bag"};
+            
+            std::cout << "Rolling...\n";
+            
+            for (int i = 0; i < 3; i++){
+                std::cout << icons[generate_random_number(0, sizeof(icons)/sizeof(std::string) - 1)] << "\t\t";
+            }
         }
     };
 }
