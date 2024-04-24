23449a42b7eb537eb941a985277590d55f5c8a2c
Flamesprinter <122485812+Flamesprinter@users.noreply.github.com>
2024-04-24 18:38:47 +0000

    Fixed error in Deck.cpp adding random now

--- src/include/Deck.cpp
+++ src/include/Deck.cpp
@@ -5,8 +5,9 @@ std::string numbers = 0;
 std::string suits [4] ={"Hearts","Diamonds","Spades","Clubs"};
 
 namespace Deck {
-  suits[rand() % 6] = std::string suit; 
+  class Card {
+   std::string suit = suits[rand() % 6]; 
 
-
+  };
    
- }
+} 
