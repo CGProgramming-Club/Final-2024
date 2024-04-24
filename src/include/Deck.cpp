23ac078f379b8a09dff51e4dcf48f96e677f5d6b
Flamesprinter <122485812+Flamesprinter@users.noreply.github.com>
2024-04-24 18:41:05 +0000

    Working on fixes to deck

--- src/include/Deck.cpp
+++ src/include/Deck.cpp
@@ -1,13 +1,13 @@
 #include "../include/Deck.hpp"
 
 
-std::string numbers = 0;
+std::string numbers = [];
 std::string suits [4] ={"Hearts","Diamonds","Spades","Clubs"};
 
 namespace Deck {
   class Card {
    std::string suit = suits[rand() % 6]; 
-
+   std::string name = 
   };
    
 } 
